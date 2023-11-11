fb9ccf90a522201876c48ea3a03f13d96f693894
Kent <comppower15@gmail.com>
2023-11-11 17:08:40 -0500

    Begins work on deck editor card pool

--- client/src/DeckEditor/DeckEditor.tsx
+++ client/src/DeckEditor/DeckEditor.tsx
@@ -2,16 +2,27 @@ import { MAIN_MENU } from 'common/Routes';
 import { useNavigate } from 'react-router-dom';
 
 import css from './DeckEditor.module.css';
+import { UnitCard } from '../game/board/cards/UnitCard';
 
 
 export default function DeckEditor() {
     const navigate = useNavigate();
-
+    let testCard = {
+        name: `Sheoldred, The Apocolypse`,
+        description: "Breaks standard", cost: "5",
+        type: "Unit", subtype: "Insectoid Horror",
+        value: "A", attack: "4", health: "5", move: "1"
+    };
+    // TODO we need to create a 
+    let cardInstances = []
+    for(let i = 0; i < 50; i++) {
+        cardInstances.push(<div className={css.cardPreview} ><UnitCard  {...testCard} /></div>)
+    }
     return (
         <div className={css.container}>
             <div className={css.navBar}>
                 <div className={css.title}>
-                    <button className={css.titleText}
+                <button className={css.button + " " + css.back}
                         onClick={() => { navigate(MAIN_MENU)}}>{'< Menu'}</button>
                     <div className={css.titleText}>Deck Editor</div>
                 </div>
@@ -21,7 +32,7 @@ export default function DeckEditor() {
                     <button className={css.button + " " + css.submit}>Save Deck</button>
                 </div>
             </div>
-            <div className={css.cardPool}></div>
+            <div className={css.cardPoolView}>{cardInstances}</div>
             <div className={css.deckMetadata}>
                 <div className={css.sizeMetadata}>
                     40 card(s)
